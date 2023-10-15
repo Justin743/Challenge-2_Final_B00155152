551e45717b2d99c5e220110922747d1ffbaa8912
Justin743 <B00155152@mytudublin.ie>
2023-10-15 16:10:40 +0100

    add cmmit

---
+++ Challenge-2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogDelay = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        dogDelay -= 0.1f;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogDelay <= 0.0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogDelay = 10.0f;
+        }
+    }
+}
